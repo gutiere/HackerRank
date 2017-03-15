9e4159c886a777e1dadb496c636aa2b04b984d45
Elijah <EdgardoGutierrezJr@gmail.com>
2017-03-15 15:42:49 -0700

    Comments added

--- SherlockandAnagrams/SherlockAndAnagrams.py
+++ SherlockandAnagrams/SherlockAndAnagrams.py
@@ -9,34 +9,70 @@
 # 4) Sum all pairs.
 
 def main():
+
+    # Total number of cases.
     cases = int(raw_input())
 
+    # Loop through all cases.
     for _ in range(cases):
+        # Get get next string.
         string = str(raw_input())
-        print(calculateTotalPairs(getSubstrings(string)))
 
-def getSubstrings(string):
-    substrings = dict()
+        # Get dictionary of anagrams and their recurrences.
+        substrings = getAnagrams(string);
+
+        # Get total pairs of anagrams.
+        totalPairs = calculateTotalPairs(substrings)
+
+        # Total pairs.
+        print(totalPairs)
+
+# Returns a dictionary of anagrams and their recurrence quantites.
+def getAnagrams(string):
+
+    # Dictionary where key = anagram, value = anagram recurrence.
+    getAnagrams = dict()
+
     for i in range(0, len(string) + 1):
         for j in range(i + 1, len(string) + 1):
+
+            # Alphabetically sorted substrings.
             anagram = ''.join(sorted(string[i:j]))
-            if (anagram in substrings):
-                substrings[anagram] = substrings[anagram] + 1
+
+            # Anagram already exists.
+            if (anagram in getAnagrams):
+                getAnagrams[anagram] = getAnagrams[anagram] + 1
+
+            # New anagram.
             else:
-                substrings[anagram] = 1
-    return substrings
+                getAnagrams[anagram] = 1
+    return getAnagrams
 
-def calculateTotalPairs(substrings):
-    total = 0
-    for key in substrings:
-        total = total + totalPairsForDuplicates(substrings[key])
-    return total
+# Returns the total number of pairs given a the quantities of recurrences.
+def calculateTotalPairs(anagrams):
 
-def totalPairsForDuplicates(number):
-    total = 0
-    for x in range(1, number):
-        total = total + x
-    return total
+    totalPairs = 0
+
+    # Loop through all anagrams.
+    for anagram in anagrams:
+
+        # Sum all anagram pairs.
+        totalPairs = totalPairs + totalPairsForRecurrences(anagrams[anagram])
+
+    return totalPairs
+
+# Calculate the total pairs for all recurrences.
+def totalPairsForRecurrences(recurrences):
+
+    totalPairs = 0
+
+    # Loop through every word and total all pairs per node (recurrence).
+    for x in range(recurrences):
+
+        # Sum all recurrence pairs.
+        totalPairs = totalPairs + x
+
+    return totalPairs
 
 if __name__ == "__main__":
     main()
