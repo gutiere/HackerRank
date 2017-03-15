34f311d369cd9d8cdb39a814c8efb36ea41a6919
Elijah <EdgardoGutierrezJr@gmail.com>
2017-03-15 07:31:10 -0700

    Optimized

--- SherlockandAnagrams/SherlockAndAnagrams.py
+++ SherlockandAnagrams/SherlockAndAnagrams.py
@@ -1,44 +1,42 @@
+# Problem: https://www.hackerrank.com/challenges/sherlock-and-anagrams
+# Solved by: Edgardo (Elijah) Gutierrez
+# Date: 3/15/17
+
+# Algorithm
+# 1) Find all substrings through nested for loop.
+# 2) Record total anagram equivelencies in dictionary.
+# 3) Calculate total pairs per total anagram equivelencies.
+# 4) Sum all pairs.
+
 def main():
     cases = int(raw_input())
 
     for _ in range(cases):
         string = str(raw_input())
-        # string = 'pvmupwjjjf'
-
-        # Get all possible substrings of variable 'string'.
-        subStrings = getSubstrings(string)
-
-        # Get the number of anagram substring pairs in list.
-        pairs = findPairs(subStrings)
-        print(pairs)
+        print(calculateTotalPairs(getSubstrings(string)))
 
 def getSubstrings(string):
-    substrings = []
+    substrings = dict()
     for i in range(0, len(string) + 1):
         for j in range(i + 1, len(string) + 1):
-            substrings.append(string[i:j])
+            anagram = ''.join(sorted(string[i:j]))
+            if (anagram in substrings):
+                substrings[anagram] = substrings[anagram] + 1
+            else:
+                substrings[anagram] = 1
     return substrings
 
-def findPairs(subStrings):
-    pairs = 0
-    for i in range(0, len(subStrings)):
-            for j in range(i + 1, len(subStrings)):
-                if (compareSubstrings(subStrings[i], subStrings[j])):
-                    pairs = pairs + 1
-    return pairs
+def calculateTotalPairs(substrings):
+    total = 0
+    for key in substrings:
+        total = total + totalPairsForDuplicates(substrings[key])
+    return total
 
-def compareSubstrings(sub1, sub2):
-    return ''.join(sorted(sub1)) == ''.join(sorted(sub2))
-
-
-
-
-
-
-
-
-
-
+def totalPairsForDuplicates(number):
+    total = 0
+    for x in range(1, number):
+        total = total + x
+    return total
 
 if __name__ == "__main__":
     main()
