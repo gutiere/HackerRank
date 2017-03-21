95e99a7e13ae25a5f4aad35e6bc8ff4d7cb800f5
Elijah <EdgardoGutierrezJr@gmail.com>
2017-03-21 16:29:25 -0700

    Lower Bound of shifting changed

--- BearAndSteadyGene/BearAndSteadyGene.py
+++ BearAndSteadyGene/BearAndSteadyGene.py
@@ -12,30 +12,30 @@ def main():
 
 # Returns true if the replacing this subgene can create a stable gene.
 def possiblility(gene, subgene):
-
     diff = gene.replace(subgene, '', 1)
-
-    letters = {'A': diff.count('A'),
-            'C': diff.count('C'),
-            'T': diff.count('T'),
-            'G': diff.count('G')}
-
-    difference = 0
-    highest = max(letters, key=letters.get)
-    for letter in letters:
-        difference = difference + letters[highest] - letters[letter]
-    return difference == len(subgene)
+    return difference(diff) == len(subgene)
 
 def findAllSubstrings(gene):
     if gene.count('A') == gene.count('C') == gene.count('T') == gene.count('G'):
         return 0
 
     # Loop through every possible subgene length, 1 to the length of the gene.
-    for subLength in range(1, len(gene) + 1):
+    for subLength in range(difference(gene) / 4, len(gene) + 1):
         for shift in range(len(gene) - subLength + 1):
             if possiblility(gene, gene[shift:shift + subLength]):
                 return subLength
     return -1
+def difference(gene):
+    letters = {'A': gene.count('A'),
+            'C': gene.count('C'),
+            'T': gene.count('T'),
+            'G': gene.count('G')}
+
+    difference = 0
+    highest = max(letters, key=letters.get)
+    for letter in letters:
+        difference = difference + letters[highest] - letters[letter]
+    return difference
 
 if __name__ == "__main__":
     main()
